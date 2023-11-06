cb0e68b610ae82ffbd830fb8e87e219f304ef831
miray-mustafov <mustafovmiray@gmail.com>
2023-11-06 10:34:22 +0200

    no more that t coins

--- 06.coin_change2.py
+++ 06.coin_change2.py
@@ -21,4 +21,27 @@ def coinChangeExactlyTCoins(n, t, coins):
     return dp[n][t]
 
 
+def coinChangeNoMoreThTCoins(n, t, coins):
+    dp = [[0 for _ in range(t + 1)] for _ in range(n + 1)]
+
+    for j in range(t + 1):
+        dp[0][j] = 1
+    # dp[0][0] = 1
+
+    for i in range(1, n + 1):
+        for j in range(1, t + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i][j] += dp[i - coin][j - 1]
+
+    return dp[n][t]
+
+
+print(coinChangeExactlyTCoins(0, 1, [1, 2, 3, 5]))  # 0
+print(coinChangeNoMoreThTCoins(0, 1, [1, 2, 3, 5]))  # 1
+
 print(coinChangeExactlyTCoins(7, 3, [1, 2, 3, 5]))  # 9
+print(coinChangeNoMoreThTCoins(7, 3, [1, 2, 3, 5]))  # 11
+
+print(coinChangeExactlyTCoins(4, 3, [1, 2, 3, 5]))  # 3
+print(coinChangeNoMoreThTCoins(4, 3, [1, 2, 3, 5]))  # 6
